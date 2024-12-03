2e391729a52dd0c032afe869c44b9938a6a32004
LunaAlma <isikpoyraz@gmail.com>
2024-12-03 16:32:17 +0100

    cleaned up the files, wrote the workers, getting a compilation error tho. I couldn't figure it out

--- interprocess/worker_s1.c
+++ interprocess/worker_s1.c
@@ -1,31 +1,15 @@
-/* 
- * Operating Systems  (2INCO)  Practical Assignment
- * Interprocess Communication
- *
- * STUDENT_NAME_1 (STUDENT_NR_1)
- * STUDENT_NAME_2 (STUDENT_NR_2)
- *
- * Grading:
- * Your work will be evaluated based on the following criteria:
- * - Satisfaction of all the specifications
- * - Correctness of the program
- * - Coding style
- * - Report quality
- * - Deadlock analysis
- */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <errno.h>      // for perror()
-#include <unistd.h>     // for getpid()
-#include <mqueue.h>     // for mq-stuff
-#include <time.h>       // for time()
-
+#include <errno.h>
+#include <unistd.h>
+#include <mqueue.h>
+#include <time.h>
 #include "messages.h"
 #include "service1.h"
 
-static void rsleep (int t);
+static void rsleep(int t);
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -36,18 +20,18 @@ int main(int argc, char *argv[]) {
     const char *s1_queue_name = argv[1];
     const char *rsp_queue_name = argv[2];
 
-    mqd_t s1_queue, rsp_queue; // Message queue descriptors
-    struct mq_attr attr;       // Queue attributes
-    Message msg;               // Message structure
+    mqd_t s1_queue, rsp_queue;
+    Message req_msg;
+    Message rsp_msg;
 
-    // Open the S1 message queue for reading
+    // Open the S1 queue for reading
     s1_queue = mq_open(s1_queue_name, O_RDONLY);
     if (s1_queue == (mqd_t)-1) {
         perror("mq_open S1_queue");
         exit(EXIT_FAILURE);
     }
 
-    // Open the Rsp message queue for writing
+    // Open the Rsp queue for writing
     rsp_queue = mq_open(rsp_queue_name, O_WRONLY);
     if (rsp_queue == (mqd_t)-1) {
         perror("mq_open Rsp_queue");
@@ -55,55 +39,45 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    printf("Service 1 Worker: Listening for jobs...\n");
+    printf("Service 1 Worker: Started processing jobs...\n");
 
     while (true) {
-        // Receive a job from the S1 queue
-        ssize_t bytes_read = mq_receive(s1_queue, (char *)&msg, sizeof(msg), NULL);
+        ssize_t bytes_read = mq_receive(s1_queue, (char *)&req_msg, sizeof(req_msg), NULL);
         if (bytes_read == -1) {
             perror("mq_receive");
             break;
         }
 
-        printf("Service 1 Worker: Received job %d with data %d\n", msg.jobID, msg.data);
+        printf("Service 1 Worker: Received job ID %d with data %d\n", req_msg.RequestID, req_msg.data);
 
-        // Process the job
-        int result = service(msg.data);
-        printf("Service 1 Worker: Processed job %d, result = %d\n", msg.jobID, result);
+        // Process the request
+        int result = service(req_msg.data);
+        rsp_msg.ResponseID = req_msg.RequestID;
+        rsp_msg.result = result;
 
-        // Send the result back to the Rsp queue
-        msg.data = result; // Replace input data with the result
-        if (mq_send(rsp_queue, (const char *)&msg, sizeof(msg), 0) == -1) {
+        rsleep(10000);  // Simulate processing time
+
+        // Send the response
+        if (mq_send(rsp_queue, (const char *)&rsp_msg, sizeof(rsp_msg), 0) == -1) {
             perror("mq_send");
             break;
         }
 
-        printf("Service 1 Worker: Sent result for job %d\n", msg.jobID);
+        printf("Service 1 Worker: Completed job ID %d with result %d\n", rsp_msg.ResponseID, rsp_msg.result);
     }
 
-    // Cleanup
     mq_close(s1_queue);
     mq_close(rsp_queue);
-
+    printf("Service 1 Worker: Exiting...\n");
     return 0;
 }
 
-
-/*
- * rsleep(int t)
- *
- * The calling thread will be suspended for a random amount of time
- * between 0 and t microseconds
- * At the first call, the random generator is seeded with the current time
- */
-static void rsleep (int t)
-{
+static void rsleep(int t) {
     static bool first_call = true;
-    
-    if (first_call == true)
-    {
-        srandom (time (NULL) % getpid ());
+
+    if (first_call) {
+        srandom(time(NULL) % getpid());
         first_call = false;
     }
-    usleep (random() % t);
+    usleep(random() % t);
 }
--- interprocess/worker_s2.c
+++ interprocess/worker_s2.c
@@ -1,69 +1,83 @@
-/* 
- * Operating Systems  (2INCO)  Practical Assignment
- * Interprocess Communication
- *
- * STUDENT_NAME_1 (STUDENT_NR_1)
- * STUDENT_NAME_2 (STUDENT_NR_2)
- *
- * Grading:
- * Your work will be evaluated based on the following criteria:
- * - Satisfaction of all the specifications
- * - Correctness of the program
- * - Coding style
- * - Report quality
- * - Deadlock analysis
- */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <errno.h>      // for perror()
-#include <unistd.h>     // for getpid()
-#include <mqueue.h>     // for mq-stuff
-#include <time.h>       // for time()
-
+#include <errno.h>
+#include <unistd.h>
+#include <mqueue.h>
+#include <time.h>
 #include "messages.h"
 #include "service2.h"
 
-static void rsleep (int t);
+static void rsleep(int t);
 
-char* name = "NO_NAME_DEFINED";
-mqd_t dealer2worker;
-mqd_t worker2dealer;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <S2_queue_name> <Rsp_queue_name>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
+    const char *s2_queue_name = argv[1];
+    const char *rsp_queue_name = argv[2];
 
-int main (int argc, char * argv[])
-{
-    // TODO:
-    // (see message_queue_test() in interprocess_basic.c)
-    //  * open the two message queues (whose names are provided in the
-    //    arguments)
-    //  * repeatedly:
-    //      - read from the S2 message queue the new job to do
-    //      - wait a random amount of time (e.g. rsleep(10000);)
-    //      - do the job 
-    //      - write the results to the Rsp message queue
-    //    until there are no more tasks to do
-    //  * close the message queues
+    mqd_t s2_queue, rsp_queue;
+    Message req_msg;
+    Message rsp_msg;
 
-    return(0);
+    // Open the S2 queue for reading
+    s2_queue = mq_open(s2_queue_name, O_RDONLY);
+    if (s2_queue == (mqd_t)-1) {
+        perror("mq_open S2_queue");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open the Rsp queue for writing
+    rsp_queue = mq_open(rsp_queue_name, O_WRONLY);
+    if (rsp_queue == (mqd_t)-1) {
+        perror("mq_open Rsp_queue");
+        mq_close(s2_queue);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Service 2 Worker: Started processing jobs...\n");
+
+    while (true) {
+        ssize_t bytes_read = mq_receive(s2_queue, (char *)&req_msg, sizeof(req_msg), NULL);
+        if (bytes_read == -1) {
+            perror("mq_receive");
+            break;
+        }
+
+        printf("Service 2 Worker: Received job ID %d with data %d\n", req_msg.RequestID, req_msg.data);
+
+        // Process the request
+        int result = service(req_msg.data);
+        rsp_msg.ResponseID = req_msg.RequestID;
+        rsp_msg.result = result;
+
+        rsleep(10000);  // Simulate processing time
+
+        // Send the response
+        if (mq_send(rsp_queue, (const char *)&rsp_msg, sizeof(rsp_msg), 0) == -1) {
+            perror("mq_send");
+            break;
+        }
+
+        printf("Service 2 Worker: Completed job ID %d with result %d\n", rsp_msg.ResponseID, rsp_msg.result);
+    }
+
+    mq_close(s2_queue);
+    mq_close(rsp_queue);
+    printf("Service 2 Worker: Exiting...\n");
+    return 0;
 }
 
-/*
- * rsleep(int t)
- *
- * The calling thread will be suspended for a random amount of time
- * between 0 and t microseconds
- * At the first call, the random generator is seeded with the current time
- */
-static void rsleep (int t)
-{
+static void rsleep(int t) {
     static bool first_call = true;
-    
-    if (first_call == true)
-    {
-        srandom (time (NULL) % getpid ());
+
+    if (first_call) {
+        srandom(time(NULL) % getpid());
         first_call = false;
     }
-    usleep (random() % t);
+    usleep(random() % t);
 }
